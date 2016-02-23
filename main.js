0a4cea60de3358b23de7ab241fbdbed8faa26423
kott9ra <andhredev@gmail.com>
2016-02-23 12:51:16 +0300

    js

---
+++ main.js
@@ -0,0 +1,45 @@
+function umn() {
+	var a  = prompt('Введите первое число');
+	var b = prompt('Введите второе число');
+	if(isNaN(a)==false & isNaN(b)==false) { 
+			alert('Произведение введенных чисел ='+a*b);
+	}else{
+		alert('Введены не числа!');
+	}
+}
+
+function slow() { 
+	var a  = prompt('Введите первое число');
+	var b = prompt('Введите второе число');
+	var c = parseInt(a)+parseInt(b);
+	if(isNaN(a)==false & isNaN(b)==false) { 
+			alert('Сумма введенных чисел = '+c);
+	}else{
+		alert('Введены не числа!');
+	}
+}
+
+function del() {
+	var a  = prompt('Введите первое число');
+	var b = prompt('Введите второе число');
+	if(isNaN(a)==false & isNaN(b)==false) { 
+			alert('Частное введенных чисел = '+a/b);
+	}else{
+		alert('Введены не числа!');
+	}
+}
+
+
+function vith(){
+	var a  = prompt('Введите первое число');
+	var b = prompt('Введите второе число');
+	var c = parseInt(a) - parseInt(b);
+	if(isNaN(a)==false & isNaN(b)==false) { 
+			alert('Разность введенных чисел = '+ c);
+	}else{
+		alert('Введены не числа!');
+	}
+
+
+}
+ 		console.log('by kott9ra');
